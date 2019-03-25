d153f9a95482bf44f9409f0f4402d4f74a3fc79b
jonatas-gama <jonatasgamasouza@hotmail.com>
2019-03-25 16:57:05 -0300

    site inicial

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+/*SERVICES*/
+
+$(function () {
+    new WOW().init()
+});
+/*WORK*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+/* Team */
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
